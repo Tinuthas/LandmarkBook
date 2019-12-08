70849d2219cca6c2b021ce7d01eb005b5fd90be2
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-08 12:51:30 -0300

    add names in cell

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -11,20 +11,23 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+     var landmarkNames = [String]()
+     var landmarkImages = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
-        var landmarkNames = [String]()
+        // Landmark Book Data
+        
         landmarkNames.append("Colosseum")
         landmarkNames.append("Great Wall")
         landmarkNames.append("Kremlin")
         landmarkNames.append("Stonehenge")
         landmarkNames.append("Taj Mahal")
         
-        var landmarkImages = [UIImage]()
+       
         landmarkImages.append(UIImage(named:"colosseum")!)
         landmarkImages.append(UIImage(named:"greatwall")!)
         landmarkImages.append(UIImage(named:"kremlin")!)
@@ -36,12 +39,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = landmarkNames[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
     
     
